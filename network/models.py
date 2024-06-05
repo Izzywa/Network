e26ab4ad317038c19a75ff05a7b9a34909ee9485
Izzywa <ismail.izzatulnajwa@gmail.com>
2024-06-05 12:03:16 +0800

    initial push

---
+++ network/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    following = models.ManyToManyField("User", related_name="followers", blank=True)
+
+    def __str__(self):
+        return self.username
