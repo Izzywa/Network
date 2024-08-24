2862bb8c31725556aa7e23bdc716afc5c384644d
Izzywa <ismail.izzatulnajwa@gmail.com>
2024-08-24 11:35:44 +0800

    styled the title

--- network/static/network/new-post.js
+++ network/static/network/new-post.js
@@ -1,7 +1,17 @@
 function NewPostBtn(props) {
-    return (
-        <div className="brand">
-            <button id="new-post-btn"><i className="fa-sharp fa-solid fa-pen-fancy" id="pen-icon"> Network</i></button>
+    const [brand, setBrand] = React.useState('Network')
+
+    function changeBrand() {
+        if (brand === 'Network') {
+            setBrand('New Post');
+        } else {
+            setBrand('Network');
+        }
+    }
+    return (<>
+    <div className="brand"  onMouseEnter={changeBrand} onMouseLeave={changeBrand}>
+            <button id="new-post-btn"><i className="fa-sharp fa-solid fa-pen-fancy" id="pen-icon">{brand}</i></button>
         </div>
+    </>
     )
 }
